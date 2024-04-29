62ee82f43b81e25cac61d002df644335ca36888a
serra <serra@boten.ca>
2024-04-29 07:02:22 -0700

    add form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,43 +1,123 @@
-import { useFormik } from "formik";
-import * as Yup from "yup";
+// import { useFormik } from 'formik';
+// import * as Yup from 'yup';
 
-const Form = () => {
-  const formik = useFormik({
-    initialValues: {},
-    validationSchema: Yup.object().shape({
-      name: Yup.string()
-        .min(2, "Name is too short")
-        .required("Name is required"),
-    }),
-    onSubmit: async (values) => {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-    },
-  });
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Formik, Field, Form } from 'formik';
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <div className="mb-5">
-        <input
-          placeholder="Full Name"
-          type="text"
-          id="name"
-          className="px-3 py-2 border border-gray-300 w-full rounded-lg"
-          {...formik.getFieldProps("name")}
-        />
-        {formik.errors.name ? (
-          <div className="mt-1 text-red-600">{formik.errors.name}</div>
-        ) : null}
-      </div>
+const Register = () => (
+  <div className="mb-5">
+    <h1>Sign Up</h1>
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      {(formik) => (
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="Jane" />
 
-      <button
-        className="rounded-lg w-full px-4 py-2 bg-green-600 text-white disabled:bg-opacity-65"
-        type="submit"
-        disabled={formik.isSubmitting}
-      >
-        {formik.isSubmitting ? "Loading" : "Submit"}
-      </button>
-    </form>
-  );
-};
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
 
-export default Form;
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <button
+            className="rounded-lg w-full px-4 py-2 bg-burgundy text-white disabled:bg-opacity-65"
+            type="submit"
+          >
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+// const Register = () => (
+//   <div>
+//     <h1>Sign Up</h1>
+//     <Formik
+//       initialValues={{
+//         firstName: '',
+//         lastName: '',
+//         email: '',
+//       }}
+//       onSubmit={async (values) => {
+//         await new Promise((r) => setTimeout(r, 500));
+//         alert(JSON.stringify(values, null, 2));
+//       }}
+//     >
+//       <Form>
+//         <label htmlFor="firstName">First Name</label>
+//         <Field id="firstName" name="firstName" placeholder="Jane" />
+
+//         <label htmlFor="lastName">Last Name</label>
+//         <Field id="lastName" name="lastName" placeholder="Doe" />
+
+//         <label htmlFor="email">Email</label>
+//         <Field
+//           id="email"
+//           name="email"
+//           placeholder="jane@acme.com"
+//           type="email"
+//         />
+//         <button type="submit">Submit</button>
+//       </Form>
+//     </Formik>
+//   </div>
+// );
+
+// const Register = () => {
+//   const formik = useFormik({
+//     initialValues: {},
+//     validationSchema: Yup.object().shape({
+//       name: Yup.string()
+//         .min(2, 'Name is too short')
+//         .required('Name is required'),
+//     }),
+//     onSubmit: async (values) => {
+//       await new Promise((resolve) => setTimeout(resolve, 2000));
+//     },
+//   });
+
+//   return (
+//     <form onSubmit={formik.handleSubmit}>
+//       <div className="mb-5">
+//         <h2 className="py-8 font-semibold text-2xl">Participant Information</h2>
+//         <input
+//           placeholder="Full Name"
+//           type="text"
+//           id="name"
+//           className="px-3 py-2 border border-gray-300 w-full rounded-lg"
+//           {...formik.getFieldProps('name')}
+//         />
+//         {/* {formik.errors.name ? (
+//           <div className="mt-1 text-red-600">{formik.errors.name}</div>
+//         ) : null} */}
+//       </div>
+
+//       <button
+//         className="rounded-lg w-full px-4 py-2 bg-green-600 text-white disabled:bg-opacity-65"
+//         type="submit"
+//         disabled={formik.isSubmitting}
+//       >
+//         {formik.isSubmitting ? 'Loading' : 'Submit'}
+//       </button>
+//     </form>
+//   );
+// };
+
+export default Register;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,30 @@
-import Form from '@/components/form';
+// import Form from '@/components/form';
+import Register from '@/components/register';
 import Head from 'next/head';
 
 const HomePage = () => {
   return (
-    <main className="flex min-h-screen">
+    <main className="flex flex-col min-h-screen bg-gray-50">
       <Head>
         <title>🌲 AWG Sign Up & Waiver 🌲</title>
       </Head>
-      <div className="container max-w-[600px] mx-auto">
-        <h1 className="mb-8 py-10 font-semibold text-3xl">
+      <div className="container max-w-2xl mx-auto px-4 py-10">
+        <h1 className="mb-8 text-3xl font-semibold text-center text-gray-800">
           Welcome to Arrow Wood Games!
         </h1>
-        <Form />
+
+        <p className="mb-8 text-gray-700">
+          Please fill this out and return to us as soon as you're able so that
+          we can best plan to meet your child’s needs. We need one form for each
+          child in our programs.
+        </p>
+
+        <p className="mb-8 font-bold">
+          The form must be filled and returned before the first day of the
+          program.
+        </p>
+
+        <Register />
       </div>
     </main>
   );
